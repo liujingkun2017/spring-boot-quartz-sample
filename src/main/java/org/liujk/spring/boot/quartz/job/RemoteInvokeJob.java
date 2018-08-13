723f411455557b660730bf6ad82844c8ba84bfcd
joy <13164432553@163.com>
2018-08-13 15:15:28 +0800

    init project

--- src/main/java/org/liujk/spring/boot/quartz/job/RemoteInvokeJob.java
+++ src/main/java/org/liujk/spring/boot/quartz/job/RemoteInvokeJob.java
@@ -1,19 +1,16 @@
 package org.liujk.spring.boot.quartz.job;
 
-
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-import java.util.Date;
-
 /**
- * 将数据库重配置的任务同步到调度引擎里面去
+ * 远程调用方法job
  */
-public class LoadConfigJob implements Job {
+public class RemoteInvokeJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println(new Date() + ":start to execute task");
+
     }
 }
